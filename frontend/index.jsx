98268fc85e117b8a3c8aa8cdd018faaadeb95652
Arthur Yip <arthuryip723@gmail.com>
2018-06-03 16:06:12 -0400

    tweaked config and set up index.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,12 @@
+import { HashRouter } from 'react-router-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+// TODO Now I need to build an App
 
 document.addEventListener('DOMContentLoaded', function () {
-    ReactDOM.render((<h1>Hello Kids!</h1>), document.getElementById('root'));
+    ReactDOM.render(
+        <Provider><HashRouter><h1>Hello Kids!</h1></HashRouter></Provider>,
+        document.getElementById('root')
+    );
 });
